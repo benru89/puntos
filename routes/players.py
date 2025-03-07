6336dd4ea2b1c846c030a1a7d5d9521bfcdf6162
ruben <ruben.fernandez@addsecure.com>
2025-03-07 15:39:46 +0100

    added total points and color rankings

--- routes/players.py
+++ routes/players.py
@@ -1,10 +1,12 @@
-from flask import Blueprint, render_template, request, redirect, url_for
-from models import Player, TrainingPoints
+from flask import Blueprint, render_template, redirect, url_for, request
 from extensions import db
+from models import Player, TrainingPoints
 import datetime, calendar
+from collections import defaultdict
 
 players_bp = Blueprint('players', __name__, template_folder='../templates')
 
+@players_bp.route('/', methods=['GET'])
 @players_bp.route('/players', methods=['GET'])
 def list_players():
     year = request.args.get('year', type=int, default=datetime.date.today().year)
@@ -19,20 +21,28 @@ def list_players():
         TrainingPoints.date.between(first_day, last_day)
     ).all()
 
-    training_points = {}
-    for tp in points_query:
-        if tp.player_id not in training_points:
-            training_points[tp.player_id] = {}
-        training_points[tp.player_id][tp.date] = tp.points
+    training_points = defaultdict(dict)
+    total_points = defaultdict(int)
 
+    for tp in points_query:
+        training_points[tp.player_id][tp.date.strftime('%Y-%m-%d')] = tp.points
+        total_points[tp.player_id] += tp.points
+
+    # Ordena los jugadores por puntos totales (mayor a menor)
+    players_sorted = sorted(players, key=lambda p: total_points[p.id], reverse=True)
+
+    # Fechas de entrenamiento únicas ordenadas
     training_dates = sorted({tp.date for tp in points_query})
 
-    return render_template('players.html',
-                           players=players,
-                           training_points=training_points,
-                           training_dates=training_dates,
-                           month=month,
-                           year=year)
+    return render_template(
+        'players.html',
+        players=players_sorted,
+        training_dates=training_dates,
+        training_points=training_points,
+        total_points=total_points,
+        month=month,
+        year=year
+    )
 
 @players_bp.route('/add_player', methods=['POST'])
 def add_player():
@@ -45,9 +55,7 @@ def add_player():
 
 @players_bp.route('/delete_player/<int:player_id>', methods=['POST'])
 def delete_player(player_id):
-    player = Player.query.get_or_404(player_id)
-    # Also remove associated training points (optional but recommended)
-    TrainingPoints.query.filter_by(player_id=player.id).delete()
-    db.session.delete(player)
+    jugador = Player.query.get_or_404(player_id)
+    db.session.delete(jugador)
     db.session.commit()
     return redirect(url_for('players.list_players'))
